21be3f4912299f684d2c9e74be8e69877a542288
Carl Flippin <carlf@carlf.io>
2019-02-19 17:30:49 -0800

    Switch signing key in git config

---
+++ emacs/.emacs.d/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (plantuml-mode haskell-mode world-time-mode web-mode slime pyenv-mode restclient tldr markdown-mode json-mode flycheck-ledger ledger-mode pdf-tools auctex elpy yaml-mode go-mode puppet-mode flycheck browse-kill-ring undo-tree which-key ace-window avy yasnippet counsel-projectile projectile counsel-tramp counsel swiper ivy-hydra hydra ivy smartparens magit dashboard smart-mode-line use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
