ccfa4aac26b1507a595ed15abc77d70c0a28f081
Carl Flippin <carlf@carlf.io>
2019-06-18 09:32:02 -0700

    Switch to a base16-outrun color scheme

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -3,9 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+	("f6f5d5adce1f9a764855c9730e4c3ef3f90357313c1cae29e7c191ba1026bc15" default)))
  '(package-selected-packages
    (quote
-    (toml-mode editorconfig plantuml-mode haskell-mode world-time-mode web-mode slime pyenv-mode restclient tldr markdown-mode json-mode flycheck-ledger ledger-mode pdf-tools auctex elpy yaml-mode go-mode puppet-mode flycheck browse-kill-ring undo-tree which-key ace-window avy yasnippet counsel-projectile projectile counsel-tramp counsel swiper ivy-hydra hydra ivy smartparens magit dashboard smart-mode-line use-package))))
+	(base16-theme smart-tabs-mode toml-mode editorconfig plantuml-mode haskell-mode world-time-mode web-mode slime pyenv-mode restclient tldr markdown-mode json-mode flycheck-ledger ledger-mode pdf-tools auctex elpy yaml-mode go-mode puppet-mode flycheck browse-kill-ring undo-tree which-key ace-window avy yasnippet counsel-projectile projectile counsel-tramp counsel swiper ivy-hydra hydra ivy smartparens magit dashboard smart-mode-line use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
