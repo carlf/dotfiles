6024ecf5e969ed0dd4f2a1ab0afe692eed2f8639
Carl Flippin <carlf@carlf.io>
2021-07-13 13:57:57 -0700

    Enable org-roam

--- emacs/.emacs.d/elisp/org.el
+++ emacs/.emacs.d/elisp/org.el
@@ -9,3 +9,28 @@
 	org-startup-indented t)
   :bind (("C-c a" . org-agenda)
 	 ("C-c c" . org-capture)))
+
+(use-package org-roam
+  :straight t
+  :init
+  (setq org-roam-directory "~/Nextcloud/Documents/org-roam"
+	org-roam-db-location "~/Nextcloud/Documents/org-roam/org-roam.db"
+	org-roam-capture-templates
+	'(("d" "default" plain #'org-roam-capture--get-point
+	   "%?"
+	   :file-name "%<%Y%m%d%H%M%S>-${slug}"
+	   :head "#+title: ${title}\n#+roam_tags: "
+	   :unnarrowed t))
+	org-roam-dailies-capture-templates
+	'(("d" "default" plain #'org-roam-capture--get-point
+	   "%?"
+	   :file-name "%<%Y-%m-%d>"
+	   :head "#+title: %<%d %B %Y>\n#+startup: indent\n#+roam_tags: dailies\n"
+	   :unnarrowed t)))
+  :bind (("C-c n r" . org-roam-buffer-toggle-display)
+	 ("C-c n i" . org-roam-insert)
+	 ("C-c n /" . org-roam-find-file)
+	 ("C-c n b" . org-roam-switch-to-buffer)
+	 ("C-c n d" . org-roam-find-directory)
+	 ("C-c n n" . org-roam-dailies-find-today))
+  :hook (after-init-hook . org-roam-mode))
