f3feb562ac70794e4be5f7eec13cd48d14ad4f99
Carl Flippin <carlf@carlf.io>
2021-08-09 14:15:22 -0700

    Install gopass automatically

--- run_30_pass.sh
+++ run_30_pass.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if [[ ! -f ~/go/bin/gopass ]]; then
+    go install github.com/gopasspw/gopass@latest
+fi
+
 if [[ ! -d ~/.password-store ]]; then
     git clone git@gitea.carlf.io:hephaestus/pass.git ~/.password-store
 fi
