37e3987cdd234588ee689cd43870001ed6e173bd
Carl Flippin <carlf@carlf.io>
2021-08-19 13:10:14 -0700

    Run emacs daemon on login

--- run_70_doom_emacs.sh
+++ run_70_doom_emacs.sh
@@ -6,11 +6,31 @@ if [[ $os == "Darwin" ]]; then
         brew tap d12frosted/emacs-plus
         brew install emacs-plus@28 --with-native-comp
     fi
+
+    daemon_status=$(brew services list | grep emacs | awk '{print $2}')
+    if [[ ! $daemon_status == "started" ]]; then
+        brew service start emacs-plus@28
+    fi
 elif [[ $os == "Linux" ]]; then
     distro=$(lsb_release -is)
     if [[ $distro == "Ubuntu" ]]; then
        if [[ ! -x /snap/bin/emacs ]]; then
            sudo snap install emacs --edge --classic
+       fi
+
+       if [[ ! -f ~/.config/systemd/user/emacs.service ]]; then
+           mkdir -p ~/.config/systemd/user
+           cp /snap/emacs/current/usr/lib/systemd/user/emacs.service ~/.config/systemd/user/
+       fi
+
+       systemctl --quiet --user is-enabled emacs.service
+       if [[ $? -ne 0 ]]; then
+           systemctl --user enable emacs.service
+       fi
+
+       systemctl --quiet --user is-active emacs.service
+       if [[ $? -ne 0 ]]; then
+           systemctl --user start emacs.service
        fi
     fi
 fi
