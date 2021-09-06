29be7dace36f859a29466ccd40282aeeed610ab4
Carl Flippin <carlf@carlf.io>
2021-09-06 16:48:36 -0700

    Switch to emacs 28

--- run_70_doom_emacs.sh
+++ run_70_doom_emacs.sh
@@ -5,7 +5,7 @@ os=$(uname -s)
 if [[ $os == "Darwin" ]]; then
     if [[ ! -x /usr/local/bin/emacs ]]; then
         brew tap d12frosted/emacs-plus
-        brew install emacs-plus@27
+        brew install emacs-plus@28
     fi
 elif [[ $os == "Linux" ]]; then
     distro=$(lsb_release -is)
