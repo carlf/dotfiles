ad087a790f06c92cfd1de90451f7ea4f0c0c0e7c
Carl Flippin <carlf@carlf.io>
2021-09-07 00:55:10 -0700

    Add hugo support

--- dot_doom.d/init.el
+++ dot_doom.d/init.el
@@ -150,7 +150,8 @@
        ;;ocaml             ; an objective camel
        (org                ; organize your plain life in plain text
         +roam2
-        +present)
+        +present
+        +hugo)
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
