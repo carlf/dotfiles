77b86199bfd8c7fc769b7db09e2cb6997ef7db62
Carl Flippin <carlf@carlf.io>
2021-09-14 14:36:01 -0400

    Set main as default branch in gitconfig.

--- dot_doom.d/init.el
+++ dot_doom.d/init.el
@@ -76,7 +76,7 @@
        ;;eshell            ; the elisp shell that works everywhere
        ;;shell             ; simple shell REPL for Emacs
        ;;term              ; basic terminal emulator for Emacs
-       ;;vterm             ; the best terminal emulation in Emacs
+       vterm               ; the best terminal emulation in Emacs
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
@@ -175,7 +175,7 @@
        ;;zig               ; C, but simpler
 
        :email
-       ;;(mu4e +org)
+       (mu4e +org)
        ;;notmuch
        ;;(wanderlust +gmail)
 
