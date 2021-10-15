3cc89383cd2cb3ad0ca2150d75d4e3c637ff0661
Carl Flippin <carlf@carlf.io>
2021-10-15 14:24:27 -0700

    Try to get projectile marked up with marginalia

--- run_70_emacs.sh
+++ run_70_emacs.sh
@@ -15,24 +15,3 @@ elif [[ $os == "Linux" ]]; then
         fi
     fi
 fi
-
-os=$(uname -s)
-if [[ $os == "Darwin" ]]; then
-    daemon_status=$(brew services list | grep emacs | awk '{print $2}')
-    if [[ ! $daemon_status == "started" ]]; then
-        brew services start emacs-plus@28
-    fi
-elif [[ $os == "Linux" ]]; then
-    distro=$(lsb_release -is)
-    if [[ $distro == "Ubuntu" ]]; then
-        systemctl --quiet --user is-enabled emacs.service
-        if [[ $? -ne 0 ]]; then
-            systemctl --user enable emacs.service
-        fi
-
-        systemctl --quiet --user is-active emacs.service
-        if [[ $? -ne 0 ]]; then
-            systemctl --user start emacs.service
-        fi
-    fi
-fi
