3a76cb6a5e373a84dc99d6ab1c9cbb52ebbd9040
Carl Flippin <carlf@carlf.io>
2021-10-25 17:39:48 -0700

    Bunch of changes. Need to make smaller changes.

--- .chezmoitemplates/gopass_install.sh
+++ .chezmoitemplates/gopass_install.sh
@@ -1,7 +1,3 @@
 if [[ ! -f ~/go/bin/gopass ]]; then
     go install github.com/gopasspw/gopass@latest
 fi
-
-if [[ ! -d ~/.password-store ]]; then
-    git clone git@gitea.carlf.io:hephaestus/pass.git ~/.password-store
-fi
